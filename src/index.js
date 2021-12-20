530250ed24064cbee4aee0700344a9eeb2caaaed
sdCaramel302812 <kypss93097@gmail.com>
2021-12-20 17:20:25 -0500

    Composition

--- src/index.js
+++ src/index.js
@@ -7,12 +7,37 @@ import {Clock} from './component.js';
 import {Toggle} from './event_handler.js';
 import {NumberLists} from './list.js';
 
+function FancyBorder(props) {
+  return (
+    <div className='FancyBorder'>
+      {props.children}
+      {props.first}
+      {props.second}
+    </div>
+  );
+}
 
+function Dialog(props) {
+  return (
+    <FancyBorder
+      first={<p>first line</p>}
+      second={<p>second line</p>}
+    >
+      <h1>{props.title}</h1>
+    </FancyBorder>
+  );
+}
 
+function WelcomeDialog() {
+  return (
+    <Dialog title="Welcome Dialog"/>
+  );
+}
 
 function App(props) {
   return (
     <div>
+      <WelcomeDialog/>
       <LoginControl/>
       <WarningBanner
         warn={true}
